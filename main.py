8fc34c135747ea5faffd8e95b27470ce05ec742b
Kristaps Ālmanis <kristapsalmanis@Kristapss-MacBook-Air-2.local>
2023-02-06 20:57:21 +0200

    Feature to send mail when new deals appear

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import fetch
 import json
+from send_mail import send
 #TODO fix The Devil In Me
 #TODO add scheduling
 #TODO add oculus store
 #TODO add steam
 
-
 with open("games.json", "r") as file:
     games = json.load(file)
-
-
-
-
 ps_games = games["PS4"]
 
+
+message = "These games from your wishlist have good deals:\n"
 for game in ps_games:
     if game["best price"] is None:
         game["best price"] = fetch.ps4_best(game["name"])
@@ -21,13 +19,12 @@ for game in ps_games:
     game["price"], game["price plus"] = fetch.ps4(game["name"])
 
     if game["price"] == game["best price"]:
-        #TODO SEND MESSAGE
-        print(1)
+        message += "\n\t" + game["name"] + " is now " + game["price"] + "\n"
     elif game["price"] < game["best price"]:
         game["best price"] = game["price"]
-        #TODO SEND MESSAGE
+        message += "\n\t" + "ALL TIME LOW - " + game["name"] + " is now just " + game["price"] + "\n"
 
-
+send(message, "PS4")
 
 
 with open("games.json", "w") as file:
