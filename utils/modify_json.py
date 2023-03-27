346ddec9c98597ca3b74d41c05f498d22abecf6c
Kristaps Ālmanis <kristapsalmanis@Kristapss-MacBook-Air-2.local>
2023-03-27 12:40:30 +0300

    fix

--- utils/modify_json.py
+++ utils/modify_json.py
@@ -26,23 +26,15 @@ def remove(name, games):
     return games
 
 def add(name, games, console):
-    if console == "PS4":
-        new_data = {
-            "name": name,
-            "price": None,
-            "price plus": None,
-            "best price": None,
-            "base price": None,
-            "notification": False
-        }
-    else:
-        new_data = {
-            "name": name,
-            "price": None,
-            "best price": None,
-            "base price": None,
-            "notification": False
-        }
+
+    new_data = {
+        "name": name,
+        "price": None,
+        "best price": None,
+        "base price": None,
+        "notification": False
+    }
+    
     games.append(new_data)
 
     return games
